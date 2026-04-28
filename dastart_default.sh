ab5bbaa33362c8012f04073f6ed4ec24bf4ec0dd
suxxio <suxio@proton.me>
2026-04-27 22:44:02 -0400

    Modify Neovim installation in dastart_default.sh
    Install Neovim via AppImage and update dependencies.

--- dastart_default.sh
+++ dastart_default.sh
@@ -1,7 +1,10 @@
 xset dpms 60 60 60
+modprobe fuse
 
-sudo apt install neovim git curl xclip
-rm -rf ~/.config/nvim
+sudo apt install git curl xclip
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage
+chmod +x nvim-linux-x86_64.appimage
+mv nvim-linux-x86_64.appimage /usr/local/bin/nvim
 git clone --depth=1 https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
 
