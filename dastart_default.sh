7bb23c1561f1fe2c3c3282a7175d79cde81d79fd
suxxio <suxio@proton.me>
2026-04-27 22:35:21 -0400

    Update dastart_default.sh to install neovim
    Install neovim and related tools, and set up LazyVim.

--- dastart_default.sh
+++ dastart_default.sh
@@ -1,1 +1,7 @@
-mkdir /tmp/dastart_default.sh
+xset dpms 60 60 60
+
+sudo apt install neovim git curl xclip
+rm -rf ~/.config/nvim
+git clone --depth=1 https://github.com/LazyVim/starter ~/.config/nvim
+rm -rf ~/.config/nvim/.git
+
