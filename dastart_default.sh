e8eee770e0329ce1bc71fad0da12a87dfd25d5b9
quitegame <quitgame@proton.me>
2026-05-04 19:20:56 -0400

    Update dastart_default.sh

--- dastart_default.sh
+++ dastart_default.sh
@@ -12,9 +12,20 @@ echo "2" >> "$LOG"
 
 apt install -y git curl xclip ripgrep fd-find
 
+# relocate my daily scripts
+PER_SRC="/home/daos/dainit/daspaces"
+PER_DEST="/home/daos"
+
+if [ -d "$PER_SRC" ]; then
+    cp -fr "$PER_SRC"/. "$PER_DEST"/
+else
+    echo "Directory does not exist: $PER_SRC" >> "$LOG"
+fi
+
 # download neovim
-curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.appimage
-chmod +x nvim-linux-x86_64.appimage
+cd /home/daos/
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+tar xzf nvim-linux-x86_64.tar.gz
 
 mv nvim-linux-x86_64.appimage /usr/local/bin/nvim.appimage
 ln -sf /usr/local/bin/nvim.appimage /usr/local/bin/nvim
