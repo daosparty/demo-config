2318dcd686fd52332756be3beedd83a3bd66782d
dangerousdance <dangerousdance@proton.me>
2026-05-16 03:33:09 +0000

    Change shell from sh to bash and update paths

--- dastart_default.sh
+++ dastart_default.sh
@@ -1,14 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
 LOG="/var/log/daos/democonfig.log"
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+
+# Define home folders
+DISTRO_HOME="/home/daos"
+MY_HOME="/home/da"
 
 echo "1" > "$LOG"
 
 # relocate my daily scripts
 PER_SRC="/tmp/dainit/daspaces"
-PER_DEST="/home/daos"
+PER_DEST="$DISTRO_HOME"
 
 if [ -d "$PER_SRC" ]; then
     cp -fr "$PER_SRC"/. "$PER_DEST"/
@@ -17,7 +22,7 @@ else
 fi
 
 {
-GITCONFIG="/home/da/.gitconfig"
+GITCONFIG="$MY_HOME/.gitconfig"
 cat > "$GITCONFIG" <<EOF
 [user]
     name = daos
@@ -28,31 +33,31 @@ chmod 644 "$GITCONFIG"
 
 echo "2" >> "$LOG"
 
-cd /home/daos/
+cd "$DISTRO_HOME"
 mkdir -p "runCommand"
 
 {
-cp /home/daos/default/i3/launcher /home/daos/.i3/
-chmod +x /home/daos/.i3/*
+cp "$SCRIPT_DIR/i3/launcher" "$DISTRO_HOME/.i3/"
+chmod +x "$DISTRO_HOME/.i3/"*
 } || true
 
-if [ -f /home/daos/default/ffprofile ]; then
-    mkdir -p /home/daos/.config
-    mv /home/daos/default/ffprofile /home/daos/.config/
+if [ -d "$SCRIPT_DIR/ffprofile" ]; then
+    mkdir -p "$DISTRO_HOME/.config"
+    mv "$SCRIPT_DIR/ffprofile" "$DISTRO_HOME/.config/"
 fi
 
 {
-cp /home/daos/default/runCommand/* /home/daos/runCommand/
-chmod +x /home/daos/runCommand/*
+cp "$SCRIPT_DIR/runCommand/"* "$DISTRO_HOME/runCommand/"
+chmod +x "$DISTRO_HOME/runCommand/"*
 
-cat > /home/daos/runCommand/.bashrc <<'EOF'
-export PATH=$PATH:/home/daos/runCommand
+cat > "$DISTRO_HOME/runCommand/.bashrc" <<EOF
+export PATH=\$PATH:$DISTRO_HOME/runCommand
 EOF
 } || true
 
 {
-grep -qxF 'source /home/daos/runCommand/.bashrc' /home/da/.bashrc || \
-echo 'source /home/daos/runCommand/.bashrc' >> /home/da/.bashrc
+grep -qxF "source $DISTRO_HOME/runCommand/.bashrc" "$MY_HOME/.bashrc" || \
+echo "source $DISTRO_HOME/runCommand/.bashrc" >> "$MY_HOME/.bashrc"
 } || true
 
 {
@@ -60,15 +65,15 @@ curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x8
 tar xzf nvim-linux-x86_64.tar.gz
 } || true
 
-chown -R 1000:1000 /home/daos
-chown -R 1000:1000 /home/da
+chown -R 1000:1000 "$DISTRO_HOME"
+chown -R 1000:1000 "$MY_HOME"
 
 
 apt install -y git curl xclip ripgrep fd-find
 
-if git clone --depth=1 https://github.com/LazyVim/starter /home/da/.config/nvim; then
-    rm -rf /home/da/.config/nvim/.git
-    chown -R 1000:1000 /home/da/.config
+if git clone --depth=1 https://github.com/LazyVim/starter "$MY_HOME/.config/nvim"; then
+    rm -rf "$MY_HOME/.config/nvim/.git"
+    chown -R 1000:1000 "$MY_HOME/.config"
 else
     echo "nvim config clone error" >> "$LOG"
 fi
@@ -87,7 +92,7 @@ mv  wqy-microhei.ttc /usr/share/fonts/truetype/wqy/
 {
 DEST="/tmp/daspaces/software/firefox"
 if [ ! -x "$DEST/firefox" ]; then
-    cd /home/daos/
+    cd "$DISTRO_HOME"
     mkdir firefox
     cd firefox
     curl -LO https://raw.githubusercontent.com/daosparty/app-firefox-115/refs/heads/master/ff1
@@ -106,7 +111,8 @@ if [ ! -x "$DEST/firefox" ]; then
     fi
 fi
 } || echo "firefox install error" >> "$LOG"
-cd /home/daos/
+
+cd "$DISTRO_HOME"
 if [ -d "firefox" ]; then
     rm -fr firefox
 fi
@@ -115,4 +121,3 @@ mkdir -p /workspaces
 mkdir -p /mnt/p1
 
 echo "4" >> "$LOG"
-
