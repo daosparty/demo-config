37f01f00ff8d2b407db69dbc5d30e176f7ef4629
usclinux <usblinux@proton.me>
2026-05-27 21:35:05 +0000

    Clean up firefox installation script
    Removed commented-out code for alternative Firefox installation path.

--- dastart_default.sh
+++ dastart_default.sh
@@ -123,7 +123,6 @@ mv  wqy-microhei.ttc /usr/share/fonts/truetype/wqy/
 {
 # DEST="/tmp/daspaces/software/firefox"
 DEST="$DISTRO_HOME/.software/firefox"
-# DEST2="/tmp/daspaces/software/firefox"
 if [ ! -x "$DEST/firefox" ]; then
     cd "$DISTRO_HOME"
     mkdir -p firefox
@@ -138,9 +137,6 @@ if [ ! -x "$DEST/firefox" ]; then
     
     if [ -x "$DEST/firefox" ]; then
         echo "✔ Firefox binary found and executable" >> "$LOG"
-        # mkdir -p "$DEST2"
-        # cp -fr "$DEST"/. "$DEST2"/
-        # chown -R 1000:1000 "/tmp/daspaces"
     else
         echo "✘ Error: firefox binary not found in $DEST" >> "$LOG"
     fi
